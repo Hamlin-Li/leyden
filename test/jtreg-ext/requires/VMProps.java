a0fbbbc3f02045664db9c61f57fac8ea32c493fc
hamlin <hamlin@hamlin.eu.rivosinc.com>
2025-07-21 11:02:26 +0000

    revert accidental changes in VMProps.java

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -24,10 +24,10 @@
 package requires;
 
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -48,14 +48,15 @@ import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
+import jdk.internal.foreign.CABI;
 import jdk.internal.misc.PreviewFeatures;
-import jdk.test.lib.Container;
-import jdk.test.lib.Platform;
-import jdk.test.whitebox.WhiteBox;
 import jdk.test.whitebox.code.Compiler;
 import jdk.test.whitebox.cpuinfo.CPUInfo;
 import jdk.test.whitebox.gc.GC;
-
+import jdk.test.whitebox.WhiteBox;
+import jdk.test.lib.Platform;
+import jdk.test.lib.Container;
 
 /**
  * The Class to be invoked by jtreg prior Test Suite execution to
